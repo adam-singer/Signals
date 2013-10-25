07b0a3890250087a8f8e5da7956ffa4f5a1d4992
nbilyk <nick@nbilyk.com>
2013-10-24 19:10:39 -0700

    Copyright notice

--- lib/signals.dart
+++ lib/signals.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2013, Nicholas BIlyk.
+// All rights reserved. Use of this source code is governed by an
+// Apache 2.0 license that can be found in the LICENSE file.
+
 library signals;
 
 import "dart:collection";
--- test/test_all.dart
+++ test/test_all.dart
@@ -1,6 +1,5 @@
 import "test_signal.dart" as signal;
 
 void main() {
-
 	signal.main();
 }
