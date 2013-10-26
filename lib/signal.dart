1d55e21378e5bc549571fbf4d7b4de28ea6435bc
nbilyk <nick@nbilyk.com>
2013-10-25 21:33:03 -0700

    blix_signals library name

--- lib/signal.dart
+++ lib/signal.dart
@@ -2,7 +2,7 @@
 // All rights reserved. Use of this source code is governed by an
 // Apache 2.0 license that can be found in the LICENSE file.
 
-part of signals;
+part of blix_signals;
 
 /**
  * A type definition for a callback.
--- lib/signals.dart
+++ lib/signals.dart
@@ -2,7 +2,7 @@
 // All rights reserved. Use of this source code is governed by an
 // Apache 2.0 license that can be found in the LICENSE file.
 
-library signals;
+library blix_signals;
 
 import "dart:collection";
 
